5493d3b24208875f1b804fd013c0c8b30b54033a
Tex024 <giac.tessera@gmail.com>
2024-12-10 22:25:51 +0100

    Fix24

--- js/buttons_manager.js
+++ js/buttons_manager.js
@@ -6,7 +6,6 @@ window.addEventListener("load", () => {
     const startButton = document.getElementById("start-button");
     const introductionSection = document.getElementById("introduction-section");
     const questionsSection = document.getElementById("questions-section");
-    const resultsSection = document.getElementById("results-section");
     const backButton = document.getElementById("back-btn");
     const nextButton = document.getElementById("next-btn");
 
@@ -25,12 +24,13 @@ window.addEventListener("load", () => {
 
         // Update navigation buttons visibility
         backButton.style.display = currentQuestion > 0 ? "inline-block" : "none";
-        nextButton.style.display = isAnswerSelected() || currentQuestion === allQuestions.length ? "inline-block" : "none";
+        nextButton.style.display = isAnswerSelected() ? "inline-block" : "none";
     };
 
     // Function to check if an answer is selected for the current question
     const isAnswerSelected = () => {
         const currentQuestionDiv = document.querySelector(`#questions-div > #question[value="${currentQuestion}"]`);
+        if (!currentQuestionDiv) return false;
         return currentQuestionDiv.querySelector(".answer-btn[data-selected]") !== null;
     };
 
@@ -53,13 +53,9 @@ window.addEventListener("load", () => {
 
     // Next button functionality
     nextButton.addEventListener("click", () => {
-        const allQuestions = document.querySelectorAll("#questions-div > #question");
-        if (currentQuestion < allQuestions.length - 1 && isAnswerSelected()) {
+        if (currentQuestion < document.querySelectorAll("#questions-div > #question").length - 1 && isAnswerSelected()) {
             currentQuestion++;
             updateVisibleQuestion();
-        } else if (currentQuestion === allQuestions.length - 1 && isAnswerSelected()) {
-            questionsSection.style.display = "none";
-            resultsSection.style.display = "block";
         }
     });
 
